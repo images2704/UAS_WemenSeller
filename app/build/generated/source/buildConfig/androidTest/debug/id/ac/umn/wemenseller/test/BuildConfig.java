4856c36be073d53b90a9aa39e56629d730f8949b
Stefan Aprilio <45485778+images2704@users.noreply.github.com>
2023-06-15 19:32:36 +0700

    prepare for release + fixes in manifest

---
+++ app/build/generated/source/buildConfig/androidTest/debug/id/ac/umn/wemenseller/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package id.ac.umn.wemenseller.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "id.ac.umn.wemenseller.test";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ app/build/generated/source/buildConfig/release/id/ac/umn/wemenseller/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package id.ac.umn.wemenseller;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "id.ac.umn.wemenseller";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.0.1";
+}
